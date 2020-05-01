baf81ff1ea3cd2640244fce55bb0fff355d3f44a
ims <ims21@users.sourceforge.net>
2020-05-01 09:48:10 +0200

    Remove whitespaces

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setup_translate
 
 pkg = 'Extensions.FontInfo'
 setup (name = 'enigma2-plugin-extensions-fontinfo',
-       version = '1.16',
-       description = 'display lineheight for font size',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['*.txt', '*.png', 'locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
-      )
+	version = '1.16',
+	description = 'display lineheight for font size',
+	packages = [pkg],
+	package_dir = {pkg: 'plugin'},
+	package_data = {pkg: ['*.txt', '*.png', 'locale/*.pot', 'locale/*/LC_MESSAGES/*.mo']},
+	cmdclass = setup_translate.cmdclass, # for translation
+	)
